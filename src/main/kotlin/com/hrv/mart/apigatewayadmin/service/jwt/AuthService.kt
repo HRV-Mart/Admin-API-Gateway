ed26715365f332ec9ce79b30c760933d57fa454f
Harsh3305 <harshverma3305@gmail.com>
2023-06-13 15:46:34 +0530

    Added auth service

---
+++ src/main/kotlin/com/hrv/mart/apigatewayadmin/service/jwt/AuthService.kt
@@ -0,0 +1,20 @@
+package com.hrv.mart.apigatewayadmin.service.jwt
+
+import com.hrv.mart.apigatewayadmin.model.AuthRequest
+import org.springframework.stereotype.Service
+
+@Service
+class AuthService {
+    private val users = mutableMapOf<String, AuthRequest>()
+    fun addUser(authRequest: AuthRequest) {
+        users[authRequest.email] = authRequest
+    }
+    fun getUser(email: String) =
+        users[email]
+    fun login(authRequest: AuthRequest) =
+        if (users.containsKey(authRequest.email)) {
+            users[authRequest.email]!!.password == authRequest.password
+        } else {
+            false
+        }
+}
