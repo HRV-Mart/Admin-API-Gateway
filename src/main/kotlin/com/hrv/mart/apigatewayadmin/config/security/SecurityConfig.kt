0c537249a762af0ee35d9cb1be666b80aadb5c74
Harsh3305 <harshverma3305@gmail.com>
2023-06-13 16:23:57 +0530

    Change dir

--- src/main/kotlin/com/hrv/mart/apigatewayadmin/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/hrv/mart/apigatewayadmin/config/security/SecurityConfig.kt
@@ -1,4 +1,4 @@
-package com.hrv.mart.apigatewayadmin.config.jwt
+package com.hrv.mart.apigatewayadmin.config.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
