4d6636c07dabb1a5be71da437c03bf5dbb3b7923
Harsh3305 <harshverma3305@gmail.com>
2023-06-13 13:32:10 +0530

    First Commit

---
+++ src/main/kotlin/com/hrv/mart/apigatewayadmin/ApiGatewayAdminApplication.kt
@@ -0,0 +1,11 @@
+package com.hrv.mart.apigatewayadmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiGatewayAdminApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiGatewayAdminApplication>(*args)
+}
---
+++ src/test/kotlin/com/hrv/mart/apigatewayadmin/ApiGatewayAdminApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hrv.mart.apigatewayadmin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiGatewayAdminApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
