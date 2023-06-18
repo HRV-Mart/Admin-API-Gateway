98929719dcce746d2c6654921fcf8ec4e7cebbad
Harsh3305 <harshverma3305@gmail.com>
2023-06-18 16:13:19 +0530

    Updated auth controller

--- src/main/kotlin/com/hrv/mart/apigatewayadmin/controller/AuthController.kt
+++ src/main/kotlin/com/hrv/mart/apigatewayadmin/controller/AuthController.kt
@@ -1,17 +1,13 @@
 package com.hrv.mart.apigatewayadmin.controller
 
-import com.hrv.mart.apigatewayadmin.model.AuthRequest
 import com.hrv.mart.apigatewayadmin.service.AuthService
 import com.hrv.mart.apigatewayadmin.service.JWTService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
 import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
 
 @RestController
 @RequestMapping("/auth")
@@ -22,23 +18,10 @@ class AuthController (
 )
 {
     @PostMapping("/login")
-    fun login(@RequestBody authRequest: AuthRequest, response: ServerHttpResponse) =
+    fun login(@RequestBody appwriteJWT: String, response: ServerHttpResponse) =
         authService
-            .login(authRequest)
-            .toMono()
+            .login(appwriteJWT, response)
             .map {
-                if (it) {
-                    jwtService.createJwt(authRequest.email)
-                }
-                else {
-                    response.statusCode = HttpStatus.UNAUTHORIZED
-                    ""
-                }
+                jwtService.createJwt(it.email)
             }
-    @PostMapping("/signup")
-    fun signup(@RequestBody authRequest: AuthRequest) =
-        authService
-            .addUser(authRequest)
-            .toMono()
-            .then(Mono.just("Sign up successful"))
 }
