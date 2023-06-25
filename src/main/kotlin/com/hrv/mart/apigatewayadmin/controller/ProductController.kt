bc11f1420bbfc34e6115e5b1f00149678f11fbb1
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-06-25 22:07:10 +0530

    Update ProductController.kt

--- src/main/kotlin/com/hrv/mart/apigatewayadmin/controller/ProductController.kt
+++ src/main/kotlin/com/hrv/mart/apigatewayadmin/controller/ProductController.kt
@@ -1,7 +1,7 @@
 package com.hrv.mart.apigatewayadmin.controller
 
 import com.hrv.mart.apigatewayadmin.service.ProductService
-import com.hrv.mart.product.Product
+import com.hrv.mart.product.model.Product
 import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.web.bind.annotation.*
 import java.security.Principal
