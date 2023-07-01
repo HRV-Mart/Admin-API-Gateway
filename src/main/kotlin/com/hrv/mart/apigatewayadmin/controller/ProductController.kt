6e4f4bf6d828cdc59efc28c2a819aee50e9ca3b6
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-07-01 13:16:55 +0530

    Get all products and get product by product id end point (#41)
    * Added custom pageable dependency
    
    * Add feature in service
    
    * Added endpoint
    
    * Added get product in service
    
    * Added end-point

--- src/main/kotlin/com/hrv/mart/apigatewayadmin/controller/ProductController.kt
+++ src/main/kotlin/com/hrv/mart/apigatewayadmin/controller/ProductController.kt
@@ -1,10 +1,12 @@
 package com.hrv.mart.apigatewayadmin.controller
 
 import com.hrv.mart.apigatewayadmin.service.ProductService
+import com.hrv.mart.custompageable.model.QueryParams
 import com.hrv.mart.product.model.Product
 import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.web.bind.annotation.*
 import java.security.Principal
+import java.util.*
 
 @RestController
 @RequestMapping("/admin/product")
@@ -12,6 +14,31 @@ class ProductController (
     private val productService: ProductService
 )
 {
+    @GetMapping("/{productId}")
+    fun getProductById(
+        @PathVariable productId: String,
+        response: ServerHttpResponse
+    ) =
+        productService
+            .getProductById(
+                productId,
+                 response
+            )
+    @GetMapping
+    fun getAllProduct(
+        @RequestParam size: Optional<Long>,
+        @RequestParam page: Optional<Long>,
+        principal: Principal,
+        response: ServerHttpResponse
+    ) =
+        productService
+            .getAllProduct(
+                queryParams = QueryParams(
+                    page = page,
+                    size = size
+                ),
+                response=response
+            )
     @PostMapping
     fun createProduct(
         @RequestBody product: Product,
