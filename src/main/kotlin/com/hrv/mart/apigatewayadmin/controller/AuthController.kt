46e42ef665e4f85d90e295118570b12065b87c88
Harsh3305 <harshverma3305@gmail.com>
2023-07-14 13:35:15 +0530

    1

--- src/main/kotlin/com/hrv/mart/apigatewayadmin/controller/AuthController.kt
+++ src/main/kotlin/com/hrv/mart/apigatewayadmin/controller/AuthController.kt
@@ -2,9 +2,11 @@ package com.hrv.mart.apigatewayadmin.controller
 
 import com.hrv.mart.apigatewayadmin.service.AuthService
 import com.hrv.mart.apigatewayadmin.service.jwt.JWTService
+import com.hrv.mart.authlibrary.model.UserType
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -22,6 +24,10 @@ class AuthController (
         authService
             .login(appwriteJWT, response)
             .map {
-                jwtService.createJwt(it.email)
+                jwtService
+                    .createJwt(
+                        userId = it.email,
+                        role = UserType.ADMIN
+                    )
             }
 }
